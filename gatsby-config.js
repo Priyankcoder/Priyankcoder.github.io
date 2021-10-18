0fd3d1211a0409befb0a5682724ba9a1ca962795
priyankrastogi14 <priyankrastogi14@gmail.com>
2021-10-18 14:02:37 +0530

    chore: add seo

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@ module.exports = {
       'Priyank Rastogi is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
     siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@Priyank55561502',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'BrittanyChiang',
-        short_name: 'BrittanyChiang',
+        name: 'Priyank Rastogi',
+        short_name: 'Priyank Rastogi',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
